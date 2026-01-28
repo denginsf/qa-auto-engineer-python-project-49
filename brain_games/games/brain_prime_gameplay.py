ce6e4cc727d6a24b14130a50b30b125fd85bd184
Aleksei Dengin <wrk.dengin@gmail.com>
2026-01-28 19:46:02 +0300

    Add brain-prime gameplay

---
+++ brain_games/games/brain_prime_gameplay.py
@@ -0,0 +1,22 @@
+from random import randint
+
+
+def brain_prime_data():
+    number_for_task = randint(0, 100)
+    rule = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+    task = f'{number_for_task}'
+    root = int(number_for_task ** 0.5)
+    if number_for_task < 2:
+        correct_answer = 'no'
+    else:
+        for i in range(2, root + 1):
+            if number_for_task % i == 0:
+                correct_answer = 'no'
+                break
+        else:
+            correct_answer = 'yes'
+    return rule, task, correct_answer
+
+
+if __name__ == '__main__':
+    brain_prime_data()
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,10 @@
+from brain_games.games.brain_prime_gameplay import brain_prime_data
+from brain_games.games.engine import game_engine
+
+
+def main():
+    game_engine(brain_prime_data)
+
+
+if __name__ == "__main__":
+    main()
